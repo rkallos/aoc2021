1b85e43d2ae3ed187137bf9632f05118466006ec
Richard Kallos <rkallos@gmail.com>
2021-12-01 13:11:25 -0500

    Solve Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,18 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int =
+        input.map { it.toInt() }
+            .zipWithNext()
+            .count { (a, b) -> b > a }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>): Int =
+        input.map { it.toInt() }
+            .windowed(3, transform = { it[0] + it[1] + it[2] })
+            .zipWithNext()
+            .count { (a, b) -> b > a }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
