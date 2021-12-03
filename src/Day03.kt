2e8ff4d89159c24e9269e2894a21528d326013f3
Richard Kallos <rkallos@gmail.com>
2021-12-03 16:32:50 -0500

    Shorten Day 03 Solution

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,22 +1,15 @@
 fun main() {
+    fun Boolean.toInt() = if (this) 1 else 0
 
     fun part1(input: List<String>): Int {
         val occs = Array(input[0].length) { 0 }
-        input.forEach { item ->
-            item.map { it - '0' }.mapIndexed { idx, digit ->
-                occs[item.length - idx - 1] += digit
-            }
-        }
+        input.forEach { l -> l.map { it - '0' }.mapIndexed { i, d -> occs[l.length - i - 1] += d } }
 
-        val gamma = occs.foldIndexed(0) { i, acc, ones ->
-            if (ones > input.size / 2) acc.or(1.shl(i)) else acc
-        }
+        val gamma = occs.map { it * 2 > input.size }.foldIndexed(0) { i, acc, b -> acc.or(b.toInt().shl(i)) }
         val epsilon = gamma.inv().and(1.shl(input[0].length) - 1)
 
         return gamma * epsilon
     }
-
-    fun Boolean.toInt() = if (this) 1 else 0
 
     fun nthBitIs(i: Int, pos: Int, expect: Int): Boolean =
         i.and(1.shl(pos)).shr(pos) == expect
@@ -28,16 +21,10 @@ fun main() {
         var susO2 = input.map { it.toInt(2) }
         var susCO2 = susO2
         for (pos in (input[0].length - 1 downTo 0)) {
-            val o2Digit = (onesCount(susO2, pos) * 2 >= susO2.size).toInt()
-            val co2Digit = (onesCount(susCO2, pos) * 2 < susCO2.size).toInt()
-            susO2 = if (susO2.size > 1)
-                susO2.filter { nthBitIs(it, pos, o2Digit) }
-            else
-                susO2
-            susCO2 = if (susCO2.size > 1)
-                susCO2.filter { nthBitIs(it, pos, co2Digit) }
-            else
-                susCO2
+            susO2 = if (susO2.size == 1) susO2 else
+                susO2.filter { nthBitIs(it, pos, (onesCount(susO2, pos) * 2 >= susO2.size).toInt()) }
+            susCO2 = if (susCO2.size == 1) susCO2 else
+                susCO2.filter { nthBitIs(it, pos, (onesCount(susCO2, pos) * 2 < susCO2.size).toInt()) }
         }
 
         return susO2[0] * susCO2[0]
